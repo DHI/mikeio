570b5e809ec8fa8f274af0a26e27295e346e3250
Henrik Andersson <jan@dhigroup.com>
2019-09-05 14:46:24 +0200

    Write to existing dfsu + 3d

--- pydhi/dfsu.py
+++ pydhi/dfsu.py
@@ -28,14 +28,22 @@ class dfsu():
             Returns data (x, nt)
         """
 
-        # NOTE. Item numbers are base 0 (everything else in the dfs is base 0)
-
         # Open the dfs file for reading
         dfs = DfsuFile.Open(dfsufile)
         self._dfs = dfs
 
+
+        # NOTE. Item numbers are base 0 (everything else in the dfs is base 0)
+        item_offset = 1
+        n_items = dfs.ItemInfo.Count
+
+        # Dynamic Z is the first item in 3d files
+        if (dfs.DfsuFileType == DfsuFileType.Dfsu3DSigma) or (dfs.DfsuFileType == DfsuFileType.Dfsu3DSigmaZ):
+            item_offset = 2
+            n_items = n_items -1
+
         if item_numbers is None:
-            item_numbers = list(range(dfs.ItemInfo.Count))
+            item_numbers = list(range(n_items))
 
 
         xNum = dfs.NumberOfElements
@@ -43,7 +51,7 @@ class dfsu():
 
         deleteValue = dfs.DeleteValueFloat
 
-        n_items = len(item_numbers)
+
         data_list = []
 
         for item in range(n_items):
@@ -56,7 +64,7 @@ class dfsu():
         for it in range(nt):
             for item in range(n_items):
 
-                itemdata = dfs.ReadItemTimeStep(item_numbers[item] + 1, it)
+                itemdata = dfs.ReadItemTimeStep(item_numbers[item] + item_offset, it)
 
                 src = itemdata.Data
                 src_hndl = GCHandle.Alloc(src, GCHandleType.Pinned)
@@ -82,37 +90,86 @@ class dfsu():
         dfs.Close()
         return (data_list, time, names)
 
-    def _get_element_coords(self):
+
+    def write(self, dfsufile, data):
+        """
+        Function: write to a pre-created dfsu file.
+
+        dfsufile:
+            full path and filename to existing dfs2 file
+
+        data:
+            list of matrices. len(data) must equal the number of items in the dfs2.
+            Easch matrix must be of dimension y,x,time
+
+        usage:
+            write(filename, data) where  data(x, nt)
+
+        Returns:
+            Nothing
+
+        """
+
+        # Open the dfs file for writing
+        dfs = DfsFileFactory.DfsGenericOpenEdit(dfsufile)
+
+        n_time_steps = dfs.FileInfo.TimeAxis.NumberOfTimeSteps
+        n_items = dfs.ItemInfo.Count
+
+        deletevalue = dfs.FileInfo.DeleteValueFloat
+
+        for i in range(n_time_steps):
+            for item in range(n_items):
+                d = data[item][:, i]
+                d[np.isnan(d)] = deletevalue
+                darray = Array[System.Single](np.array(d.reshape(d.size, 1)[:, 0]))
+                dfs.WriteItemTimeStepNext(0, darray)
+
+        dfs.Close()
+
+    def get_element_coords(self):
         ne = self._dfs.NumberOfElements
 
         # Node coordinates
         xn = np.array(list(self._dfs.X))
         yn = np.array(list(self._dfs.Y))
+        zn = np.array(list(self._dfs.Z))
 
-        ec = np.empty([ne,2])
+        ec = np.empty([ne,3])
 
         for j in range(ne):
             nodes = self._dfs.ElementTable[j]
 
             xcoords = np.empty(nodes.Length)
             ycoords = np.empty(nodes.Length)
+            zcoords = np.empty(nodes.Length)
             for i in range(nodes.Length):
                 nidx = nodes[i]-1
                 xcoords[i] = xn[nidx]
                 ycoords[i] = yn[nidx]
+                zcoords[i] = zn[nidx]
 
             ec[j,0] = xcoords.mean()
             ec[j,1] = ycoords.mean()
+            ec[j,2] = zcoords.mean()
 
         return ec
 
-    def find_closest_element_index(self, x, y):
 
-        ec = self._get_element_coords()
-        poi = np.array([x,y])
+    def find_closest_element_index(self, x, y, z=None):
 
-        d = ((ec - poi)**2).sum(axis=1)
-        idx = d.argsort()[0]
+        ec = self.get_element_coords()
+
+        if z is None:
+            poi = np.array([x,y])
+
+            d = ((ec[:,0:2] - poi)**2).sum(axis=1)
+            idx = d.argsort()[0]
+        else:
+            poi = np.array([x, y, z])
+
+            d = ((ec - poi)**2).sum(axis=1)
+            idx = d.argsort()[0]
 
         return idx
 
