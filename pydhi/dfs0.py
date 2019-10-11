49b350efffcd6564cd6b698d918d8b9d4c64ebb5
Henrik Andersson <jan@dhigroup.com>
2019-10-11 10:32:12 +0200

    Dfs0 similar to other datatypes

--- pydhi/dfs0.py
+++ pydhi/dfs0.py
@@ -166,9 +166,11 @@ class dfs0():
 
         dfs.Close()
 
-    def create_equidistant_calendar(self, dfs0file, data, start_time, timeseries_unit, dt, variable_type, unit, names=None,
-                                    title=None, data_value_type=None):
-        """Create_equidistant_calendar creates a dfs0 file with Equidistant Calendar.
+    def create(self, dfs0file, data,
+               start_time=None, timeseries_unit=1400, dt=3600, datetimes=None,
+               variable_type=None, unit=None, names=None,
+               title=None, data_value_type=None):
+        """create creates a dfs0 file.
 
         dfs0file:
             Full path and filename to dfs0 to be created.
@@ -177,7 +179,7 @@ class dfs0():
         start_time:
             start date of type datetime.
         timeseries_unit:
-            second=1400, minute=1401, hour=1402, day=1403, month=1405, year= 1404
+            second=1400, minute=1401, hour=1402, day=1403, month=1405, year= 1404, default=1400
         dt:
             the time step (double based on the timeseries_unit). Therefore dt of 5.5 with timeseries_unit of minutes
             means 5 mins and 30 seconds.
@@ -199,8 +201,20 @@ class dfs0():
         if title is None:
             title = "dfs0 file"
 
-        n_items = np.shape(data)[1]
-        n_time_steps = np.shape(data)[0]
+        n_items = len(data)
+        n_time_steps = np.shape(data[0])[0]
+
+        if start_time is None:
+            start_time = datetime.now()
+
+        if names is None:
+            names = [f"Item {i+1}" for i in range(n_items)]
+
+        if variable_type is None:
+            variable_type = [999] * n_items
+
+        if unit is None:
+            unit = [0] * n_items
 
         if names is not None and len(names) != n_items:
             raise Warning(
@@ -215,8 +229,14 @@ class dfs0():
                 "unit if specified must be an array of integers (enuType) with the same number of "
                 "elements as data columns")
 
-        if not type(start_time) is datetime:
-            raise Warning("start_time must be of type datetime ")
+        if datetimes is None:
+            equidistant = True
+
+            if not type(start_time) is datetime:
+                raise Warning("start_time must be of type datetime ")
+        else:
+            start_time = datetimes[0]
+            equidistant = False
 
         if not isinstance(timeseries_unit, int):
             raise Warning("timeseries_unit must be an integer. See dfsutil options for help ")
@@ -228,8 +248,14 @@ class dfs0():
         builder = DfsBuilder.Create(title, 'DFS', 0)
         builder.SetDataType(1)
         builder.SetGeographicalProjection(factory.CreateProjectionUndefined())
-        builder.SetTemporalAxis(factory.CreateTemporalEqCalendarAxis(
-            timeseries_unit, system_start_time, 0, dt))
+
+        if equidistant:
+            builder.SetTemporalAxis(factory.CreateTemporalEqCalendarAxis(
+                timeseries_unit, system_start_time, 0, dt))
+        else:
+            builder.SetTemporalAxis(factory.CreateTemporalNonEqCalendarAxis(
+                timeseries_unit, system_start_time))
+
         builder.SetItemStatisticsType(StatType.RegularStat)
 
         for i in range(n_items):
@@ -259,106 +285,19 @@ class dfs0():
         dfs = builder.GetFile()
         delete_value = dfs.FileInfo.DeleteValueFloat
 
-        data[np.isnan(data)] = delete_value
-
-        # COPY OVER THE DATA
-        for it in range(n_time_steps):
-            for ii in range(n_items):
-                d = Array[System.Single](np.array([[data[it, ii]]]))
-                dfs.WriteItemTimeStepNext(it, d)
-
-        dfs.Close()
-
-    def create_non_equidistant_calendar(self, dfs0file, data, time_vector, variable_type, unit, names=None,
-                                        title=None, data_value_type=None):
-        """Create_non_equidistant_calendar creates a dfs0 file with NOT-Equidistant Calendar.
-
-        dfs0file: Full path and filename to dfs0 to be created.
-        data: a numpy matrix
-        time_vector: A list of datetime elements.
-        variable_type: Array integers corresponding to a variable types (ie. Water Level). Use dfsutil type_list
-            to figure out the integer corresponding to the variable.
-        unit: Array integers corresponding to the unit corresponding to the variable types The unit (meters, seconds),
-            use dfsutil unit_list to figure out the corresponding unit for the variable.
-        names: array of names (ie. array of strings)
-        title: title (string)
-        data_value_type:  Instantaneous = 0 (default), Accumulated = 1, StepAccumulated = 3, MeanStepBackward = 3,
-            MeanStepForward = 4.
-
-        """
-
-        if title is None:
-            title = "dfs0 file"
-
-        n_items = np.shape(data)[1]
-        n_time_steps = np.shape(data)[0]
-
-        if names is not None and len(names) != n_items:
-            raise Warning(
-                "names must be an array of strings with the same number of elements as data columns")
-
-        if len(variable_type) != n_items or not all(isinstance(item, int) and 0 <= item < 1e15
-                                                    for item in variable_type):
-            raise Warning("type if specified must be an array of integers (enuType) with the same number of "
-                          "elements as data columns")
-
-        if not len(time_vector) == n_time_steps or not all(isinstance(t, datetime)for t in time_vector):
-            raise Warning("The time_vector must be an array or list of datetime of same length as the number of "
-                          "rows in the data")
-
-        if len(unit) != n_items or not all(isinstance(item, int) and 0 <= item < 1e15 for item in unit):
-            raise Warning(
-                "unit if specified must be an array of integers (enuType) with the same number of "
-                "elements as data columns")
-
-        start_time = time_vector[0]
-        system_start_time = System.DateTime(start_time.year, start_time.month, start_time.day,
-                                            start_time.hour, start_time.minute, start_time.second)
-
-        # default eumUnit --> second = 1400
-        timeseries_unit = 1400
-
-        factory = DfsFactory()
-        builder = DfsBuilder.Create(title, 'DFS', 0)
-        builder.SetDataType(1)
-        builder.SetGeographicalProjection(factory.CreateProjectionUndefined())
-        builder.SetTemporalAxis(factory.CreateTemporalNonEqCalendarAxis(
-            timeseries_unit, system_start_time))
-        builder.SetItemStatisticsType(StatType.RegularStat)
-
         for i in range(n_items):
-
-            item = builder.CreateDynamicItemBuilder()
-            if type is not None:
-                item.Set(names[i], eumQuantity.Create(
-                    variable_type[i], unit[i]), DfsSimpleType.Float)
-            else:
-                item.Set(str(i), eumQuantity.Create(
-                    eumItem.eumIItemUndefined, 0), DfsSimpleType.Float)
-
-            if data_value_type is not None:
-                item.SetValueType(data_value_type[i])
-            else:
-                item.SetValueType(DataValueType.Instantaneous)
-
-            item.SetAxis(factory.CreateAxisEqD0())
-            builder.AddDynamicItem(item.GetDynamicItemInfo())
-
-        try:
-            builder.CreateFile(dfs0file)
-        except IOError:
-            print('cannot create dfso file: ', dfs0file)
-
-        dfs = builder.GetFile()
-        delete_value = dfs.FileInfo.DeleteValueFloat
-
-        data[np.isnan(data)] = delete_value
+            d = data[i]
+            d[np.isnan(d)] = delete_value
 
         # COPY OVER THE DATA
         for it in range(n_time_steps):
-            dt = (time_vector[it] - time_vector[0]).total_seconds()
             for ii in range(n_items):
-                d = Array[System.Single](np.array([[data[it, ii]]]))
-                dfs.WriteItemTimeStepNext(dt, d)
+
+                d = Array[System.Single](np.array(data[ii][it:it+1]))
+                if equidistant:
+                    dfs.WriteItemTimeStepNext(it, d)
+                else:
+                    dt = (datetimes[it] - datetimes[0]).total_seconds()
+                    dfs.WriteItemTimeStepNext(dt, d)
 
         dfs.Close()
--- tests/test_dfs0.py
+++ tests/test_dfs0.py
@@ -4,12 +4,32 @@ import datetime
 from pydhi import dfs0 as dfs0
 
 
+def test_simple_create():
+
+    dfs0File = r"simple.dfs0"
+
+    data = []
+
+    nt = 100
+    d = np.random.random([nt])
+    data.append(d)
+
+    dfs = dfs0.dfs0()
+
+    dfs.create(dfs0file=dfs0File, data=data)
+
+    assert True
+    os.remove(dfs0File)
+
 
 def test_create_equidistant_calendar():
 
     dfs0file = r'random.dfs0'
-    data = np.random.random([1000, 2])
-    data[2, :] = np.nan
+    d1 = np.random.random([1000])
+    d2 = np.random.random([1000])
+    data = []
+    data.append(d1)
+    data.append(d2)
     start_time = datetime.datetime(2017, 1, 1)
     timeseries_unit = 1402
     title = 'Hello Test'
@@ -19,21 +39,24 @@ def test_create_equidistant_calendar():
     data_value_type = [0, 1]
     dt = 5
     dfs = dfs0.dfs0()
-    dfs.create_equidistant_calendar(dfs0file=dfs0file, data=data,
-                                    start_time=start_time,
-                                    timeseries_unit=timeseries_unit, dt=dt,
-                                    names=names, title=title,
-                                    variable_type=variable_type,
-                                    unit=unit, data_value_type=data_value_type)
+    dfs.create(dfs0file=dfs0file, data=data,
+               start_time=start_time,
+               timeseries_unit=timeseries_unit, dt=dt,
+               names=names, title=title,
+               variable_type=variable_type,
+               unit=unit, data_value_type=data_value_type)
 
     os.remove(dfs0file)
     assert True
 
 
 def test_create_non_equidistant_calendar():
-    dfs0file = r'random.dfs0'
-    data = np.random.random([1000, 2])
-    data[2, :] = np.nan
+    dfs0file = r'neq.dfs0'
+    d1 = np.random.random([1000])
+    d2 = np.random.random([1000])
+    data = []
+    data.append(d1)
+    data.append(d2)
     start_time = datetime.datetime(2017, 1, 1)
     time_vector = []
     for i in range(1000):
@@ -45,11 +68,11 @@ def test_create_non_equidistant_calendar():
     data_value_type = [0, 1]
 
     dfs = dfs0.dfs0()
-    dfs.create_non_equidistant_calendar(dfs0file=dfs0file, data=data,
-                                        time_vector=time_vector,
-                                        names=names, title=title,
-                                        variable_type=variable_type, unit=unit,
-                                        data_value_type=data_value_type)
+    dfs.create(dfs0file=dfs0file, data=data,
+               datetimes=time_vector,
+               names=names, title=title,
+               variable_type=variable_type, unit=unit,
+               data_value_type=data_value_type)
 
     assert True
     os.remove(dfs0file)
