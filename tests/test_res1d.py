69ef95bff7c1c9ddf2dff9745325be0e81214bf4
rha <rha@dhigroup.com>
2020-02-25 10:04:42 +0100

    Add res1d test

---
+++ tests/test_res1d.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import datetime
+from mikeio import res1d as r1d
+
+def test_read_single_item():
+
+    file = r"testdata/Exam6Base.res1d"
+
+    p1 = r1d.ExtractionPoint()
+    p1.BranchName  = '104l1'
+    p1.Chainage = 34.4131
+    p1.VariableType  = 'WaterLevel'
+    ts = r1d.read(file, [p1])
+
+    assert len(ts) == 110
