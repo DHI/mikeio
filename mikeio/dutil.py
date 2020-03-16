c31cad0f86e5b20d92330fa070b3a4a3a19f655d
Henrik Andersson <jan@dhigroup.com>
2020-03-16 11:18:57 +0100

    Documentation

--- mikeio/dutil.py
+++ mikeio/dutil.py
@@ -2,10 +2,20 @@ import numpy as np
 from System.Runtime.InteropServices import GCHandle, GCHandleType
 import ctypes
 
-from collections import namedtuple
-
 
 def to_numpy(src):
+    """
+    Convert .NET array to numpy array
+
+    Parameters
+    ----------
+    src : System.Array
+        
+    Returns
+    -------
+    np.ndarray
+        
+    """
 
     src_hndl = GCHandle.Alloc(src, GCHandleType.Pinned)
     try:
@@ -21,16 +31,72 @@ def to_numpy(src):
 
 
 def find_item(dfs, item_names):
+    """Utility function to find item numbers
+
+    Parameters
+    ----------
+    dfs : DfsFile
+
+    item_names : list[str]
+        Names of items to be found
+
+    Returns
+    -------
+    list[int]
+        item numbers (0-based)
+
+    Raises
+    ------
+    KeyError
+        In case item is not found in the dfs file
+    """
     names = [x.Name for x in dfs.ItemInfo]
     item_lookup = {name: i for i, name in enumerate(names)}
     try:
         item_numbers = [item_lookup[x] for x in item_names]
-    except:
-        raise ValueError(f"Selected item name not found. Valid names are {names}")
+    except KeyError:
+        raise KeyError(f"Selected item name not found. Valid names are {names}")
+
     return item_numbers
 
 
 class Dataset:
+    """Dataset
+
+    Attributes
+    ----------
+    data: list[np.array]
+        Data, potentially multivariate and multiple spatial dimensions
+    time: list[datetime]
+        Datetime of each timestep
+    names: list[str]
+        Names of each item in the data list
+
+    Notes
+    -----
+    Data from a specific item can be accessed using the name of the item
+    similar to a dictionary.
+
+    Attributes data, time, names can also be unpacked like a tuple
+
+    Examples
+    --------
+    >>> ds = mikeio.read("tests/testdata/random.dfs0")
+    >>> ds
+    DataSet(data, time, names)
+    Number of items: 2
+    Shape: (1000,)
+    2017-01-01 00:00:00 - 2017-07-28 03:00:00
+    >>> ds.names
+    ['VarFun01', 'NotFun']
+    >>> ds['NotFun'][0:5]
+    array([0.64048636, 0.65325695, nan, 0.21420799, 0.99915695])
+
+    >> data,time,names = ds
+    >> names
+    ['VarFun01', 'NotFun']
+    """
+
     def __init__(self, data, time, names):
         self.data = data
         self.time = time
@@ -48,7 +114,7 @@ class Dataset:
         return str.join("\n", out)
 
     def __len__(self):
-        return 2  # [data,time,names]
+        return 3  # [data,time,names]
 
     def __getitem__(self, x):
 
