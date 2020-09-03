3b54d72a01a69fc11e41fdf82e3a465ada063d57
Henrik Andersson <jan@dhigroup.com>
2020-09-03 13:47:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mikeio",
-    version="0.5.0",
+    version="0.5.1",
     install_requires=["pythonnet", "numpy", "pandas", "matplotlib"],
     extras_require={
         "dev": ["pytest", "black", "sphinx", "sphinx", "sphinx-rtd-theme", "shapely"],
