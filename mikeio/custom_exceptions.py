b4f717aa78e90689d5422dc4e90458650e1a1b57
rha <rha@dhigroup.com>
2020-10-02 12:17:30 +0200

    Add one more exception in dfs1

--- mikeio/custom_exceptions.py
+++ mikeio/custom_exceptions.py
@@ -1,4 +1,10 @@
 class FileDoesNotExist(Exception):
     def __init__(self, filename=""):
-        self.message = f"File {filename} does not exist."
+        super().__init__(f"File {filename} does not exist.")
+
+
+class DataDimensionMismatch(ValueError):
+    def __init__(self):
+        self.message = "Data matrices in the x dimension do not all match in the data list." \
+                       "Data is a list of matrices [t, x]."
         super().__init__(self.message)
