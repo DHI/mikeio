e67e2260beb948371867222b39b60b9444275930
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2020-10-20 11:26:10 +0200

    test overset grid

--- tests/test_dfsu.py
+++ tests/test_dfsu.py
@@ -238,6 +238,31 @@ def test_contains():
     assert inside[0] == True
     assert inside[1] == False
 
+
+def test_get_overset_grid():
+    filename = os.path.join("tests", "testdata", "FakeLake.dfsu")
+    dfs = Dfsu(filename)
+
+    x0, dx, nx, y0, dy, ny = dfs.get_overset_grid()
+    assert nx == 21
+    assert ny == 10
+
+    x0, dx, nx, y0, dy, ny = dfs.get_overset_grid(dxdy=0.2)
+    assert dx == 0.2
+    assert dy == 0.2
+
+    x0, dx, nx, y0, dy, ny = dfs.get_overset_grid(dxdy=(0.4,0.2))
+    assert dx == 0.4
+    assert dy == 0.2
+
+    x0, dx, nx, y0, dy, ny = dfs.get_overset_grid(shape=(5,4))
+    assert nx == 5
+    assert ny == 4
+
+    x0, dx, nx, y0, dy, ny = dfs.get_overset_grid(shape=(None,5))
+    assert nx == 11
+    assert ny == 5
+
 def test_find_nearest_element_2d():
     filename = os.path.join("tests", "testdata", "HD2D.dfsu")
     dfs = Dfsu(filename)
