c046b10551a28b3c48c1791d899e399581bbb995
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2020-10-25 21:06:23 +0100

    Create interpolation.py

---
+++ mikeio/interpolation.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+#class Interpolation2D:
+def get_idw_interpolant(self, distances, p=1):
+    """IDW interpolant for 2d array of distances
+
+    Parameters
+    ----------
+    distances : array-like 
+        distances between interpolation point and grid point
+    p : int, optional
+        order of inverse distance weighting, default=1
+
+    Returns
+    -------
+    np.array
+        weights 
+    """
+    MIN_DISTANCE = 1e-8
+    weights = np.zeros(distances.shape)
+    p = 1   # inverse distance order 
+    
+    match = distances[:,0] < MIN_DISTANCE
+    weights[match,0] = 1
+    
+    weights[~match,:] = 1 / distances[~match,:]**p
+    denom = weights[~match,:].sum(axis=1).reshape(-1,1) #*np.ones((1,n_nearest))
+    weights[~match,:] = weights[~match,:] / denom            
+
+    return weights
+
+def interp2d(self, data, elem_ids, weights=None):
+    
+    if isinstance(data, np.ndarray):
+        if data.ndim == 1:
+            # data is single item and single time step
+            return self._interp_itemstep(data, elem_ids, weights)
+        elif data.ndim == 2:
+            # data is single item 
+            data = [data]
+
+    idat = []
+    ni = len(elem_ids)
+    for datitem in data:
+        nt, ne = datitem.shape
+        idatitem = np.empty(shape=(nt,ni)) 
+        for step in range(nt):
+            idatitem[step,:] = self._interp_itemstep(datitem[step,:], elem_ids, weights)
+        idat.append(idatitem)
+    return idat
+
+def _interp_itemstep(self, data, elem_ids, weights=None):
+    if weights is None:
+        # nearest neighbor
+        return data[elem_ids]
+    ni = len(elem_ids)
+    idat = np.empty(ni)
+    for j in range(ni):
+        idat[j] = np.dot(data[elem_ids[j]], weights[j])
+    return idat        
