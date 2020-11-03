bb740b173613c626cb97d86cf4d7b97573631dda
Henrik Andersson <jan@dhigroup.com>
2020-11-03 11:12:10 +0100

    Prepare for new release, with breaking changes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mikeio",
-    version="0.5.3",
+    version="0.6.0",
     install_requires=["pythonnet", "numpy", "pandas", "scipy", "pyyaml"],
     extras_require={
         "dev": [
