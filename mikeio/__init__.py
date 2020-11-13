2d24bd5d74f6097a7dc46d246684ac4ac4a534e9
Henrik Andersson <jan@dhigroup.com>
2020-11-13 08:29:23 +0100

    Add version

--- mikeio/__init__.py
+++ mikeio/__init__.py
@@ -3,6 +3,24 @@ import sys
 import os
 from platform import architecture
 
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.6.dev1'
+
 mike_bin_path = os.path.join(os.path.dirname(__file__), "bin")
 sys.path.append(mike_bin_path)
 
