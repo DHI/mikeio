83da210337e969e67b1ba9eb9803d83f69d97afe
Henrik Andersson <jan@dhigroup.com>
2021-05-04 20:06:45 +0200

    Res1d and Xns11 can be found next door

---
+++ mikeio/res1d.py
@@ -0,0 +1,10 @@
+"""Res1d has moved to a new repo: https://github.com/DHI/mikeio1d/"""
+
+
+class Res1d:
+    """Res1d has moved to a new repo: https://github.com/DHI/mikeio1d/"""
+
+    def __init__(self, file_path=None, put_chainage_in_col_name=True):
+        raise NotImplementedError(
+            "Res1d has moved to a new repo: https://github.com/DHI/mikeio1d/"
+        )
---
+++ mikeio/xns11.py
@@ -0,0 +1,10 @@
+"""Xns11 has moved to a new repo: https://github.com/DHI/mikeio1d/"""
+
+
+class Xns11:
+    """Xns11 has moved to a new repo: https://github.com/DHI/mikeio1d/"""
+
+    def __init__(self, file_path=None):
+        raise NotImplementedError(
+            "Xns11 has moved to a new repo: https://github.com/DHI/mikeio1d/"
+        )
