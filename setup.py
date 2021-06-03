ef52d8b76f035066e3efda88fccee82674995878
Henrik Andersson <jan@dhigroup.com>
2021-06-03 15:05:12 +0200

    Last release based on .NET

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mikeio",
-    version="0.7.dev1",
+    version="0.6.4",
     install_requires=[
         "pythonnet",
         "numpy",
