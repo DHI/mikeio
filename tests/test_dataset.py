42c05035542244efb2dd6ae74c0822d5d9543047
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-08-25 18:03:32 +0200

    add tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -11,6 +11,31 @@ from mikeio.eum import EUMType, ItemInfo, EUMUnit
 def _get_time(nt):
     return list(rrule(freq=SECONDLY, count=nt, dtstart=datetime(2000, 1, 1)))
 
+@pytest.fixture
+def ds1():
+    nt = 10
+    ne = 7
+
+    d1 = np.zeros([nt, ne]) + 0.1
+    d2 = np.zeros([nt, ne]) + 0.2
+    data = [d1, d2]
+
+    time = _get_time(nt)
+    items = [ItemInfo("Foo"), ItemInfo("Bar")]
+    return Dataset(data, time, items)
+
+@pytest.fixture
+def ds2():
+    nt = 10
+    ne = 7
+
+    d1 = np.zeros([nt, ne]) + 1.0
+    d2 = np.zeros([nt, ne]) + 2.0
+    data = [d1, d2]
+
+    time = _get_time(nt)
+    items = [ItemInfo("Foo"), ItemInfo("Bar")]
+    return Dataset(data, time, items)
 
 def test_get_names():
 
@@ -877,3 +902,29 @@ def test_init():
     assert ds.start_time == datetime(2018, 1, 1, 0, 0, 0)
     assert ds.n_elements == n_elements
     assert ds.items[0].name == "Foo"
+
+def test_add_scalar(ds1):
+    ds2 = ds1 + 10.0
+    assert np.all(ds2[0] - ds1[0] == 10.0)
+    
+    
+def test_sub_scalar(ds1):
+    ds2 = ds1 - 10.0
+    assert np.all(ds1[0] - ds2[0] == 10.0)
+
+
+def test_mul_scalar(ds1):
+    ds2 = ds1 * 2.0
+    assert np.all(ds2[0]*0.5 == ds1[0]) 
+
+
+def test_add_dataset(ds1, ds2):
+    ds3 = ds1 + ds2
+    assert np.all(ds3[0] == 1.1)
+    assert np.all(ds3[1] == 2.2)
+    
+    
+def test_sub_dataset(ds1, ds2):
+    ds3 = ds2 - ds1
+    assert np.all(ds3[0] == 0.9)
+    assert np.all(ds3[1] == 1.8)
