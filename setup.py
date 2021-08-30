7a27ec81907fc774d8ddacefd7e80cf92bb07ee3
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-08-30 09:23:30 +0200

    add rasterio, geopandas

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
             "jupyterlab",
         ],
         "test": ["pytest", "matplotlib", "shapely"],
-        "notebooks": ["ipykernel", "nbformat", "nbconvert", "jupyter", "xarray", "netcdf4"],
+        "notebooks": ["ipykernel", "nbformat", "nbconvert", "jupyter", "xarray", "netcdf4", "rasterio", "geopandas"],
     },
     author="Henrik Andersson",
     author_email="jan@dhigroup.com",
