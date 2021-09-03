76edd8a4844921449e87408131f21df77963a737
Henrik Andersson <jan@dhigroup.com>
2021-09-03 12:26:44 +0200

    Bump version to create consistent version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mikeio",
-    version="0.7.1",
+    version="0.7.2",
     install_requires=[
         "mikecore",
         "numpy",
