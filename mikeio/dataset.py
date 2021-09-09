70ff5d54bb8d2c76f41c0add8b2f025da6acdb87
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-09-09 08:58:06 +0200

    move to after dunder methods

--- mikeio/dataset.py
+++ mikeio/dataset.py
@@ -159,11 +159,6 @@ class Dataset(TimeSeries):
 
         return str.join("\n", out)
 
-    def describe(self, **kwargs):
-        """Generate descriptive statistics by wrapping pandas descibe()"""
-        all_df = [pd.DataFrame(self.data[j].flatten(), columns=[self.items[j].name]).describe(**kwargs) for j in range(self.n_items)]
-        return pd.concat(all_df, axis=1)
-
     def __len__(self):
         return len(self.items)
 
@@ -275,6 +270,11 @@ class Dataset(TimeSeries):
         items = deepcopy(self.items)
         time = self.time.copy()
         return Dataset(data, time, items)
+
+    def describe(self, **kwargs):
+        """Generate descriptive statistics by wrapping pandas describe()"""
+        all_df = [pd.DataFrame(self.data[j].flatten(), columns=[self.items[j].name]).describe(**kwargs) for j in range(self.n_items)]
+        return pd.concat(all_df, axis=1)
 
     def copy(self):
         """Returns a copy of this dataset."""
