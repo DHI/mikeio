b2e1af27be828571a4402e554dcbf48315f51892
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-09-20 09:26:11 +0200

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -49,3 +49,5 @@ html_theme = "sphinx_book_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
+
+html_logo = "../images/logo/SVG/MIKE-IO-Logo-Pos-RGB.svg"
