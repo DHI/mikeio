bb55ff47f645925b532910cb8f6dc9f83e0ea097
Henrik Andersson <jan@dhigroup.com>
2021-10-26 10:54:19 +0200

    Release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mikeio",
-    version="0.10.dev0",
+    version="0.10.1",
     install_requires=[
         "mikecore>=0.1.2",
         "numpy>=1.15.0.",  # first version with numpy.quantile
