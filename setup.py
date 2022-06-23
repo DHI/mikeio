5145ff9ee61a36ed0d5d5c37d163bd056de52340
Henrik Andersson <jan@dhigroup.com>
2022-06-23 18:02:23 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mikeio",
-    version="1.0.1",
+    version="1.0.2",
     install_requires=[
         "mikecore>=0.2.1",
         "numpy>=1.15.0",  # first version with numpy.quantile
