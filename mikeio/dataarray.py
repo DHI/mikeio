ebf1ee5299f67d7e80122fef1590e1d60bf40f94
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2022-06-24 14:50:59 +0000

    remove duplicate import

--- mikeio/dataarray.py
+++ mikeio/dataarray.py
@@ -26,7 +26,6 @@ from .spatial.FM_geometry import (
 from mikecore.DfsuFile import DfsuFileType
 from .spatial.FM_utils import _plot_map, _plot_vertical_profile
 from mikecore.DfsuFile import DfsuFileType
-from .spatial.FM_utils import _plot_map
 from .spectral_utils import plot_2dspectrum, calc_m0_from_spectrum
 from .data_utils import DataUtilsMixin
 
