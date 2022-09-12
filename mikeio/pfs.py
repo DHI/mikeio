ef33faa608c84a55a73c54842e0a24958a78f143
Clemens Cremer <clcr@dhigroup.com>
2022-09-12 14:28:18 +0200

    pfs.write functionality added
    added pfs.write functionality and demo notebook _test_pfsReadnWrite along with demo input data

--- mikeio/pfs.py
+++ mikeio/pfs.py
@@ -33,13 +33,13 @@ class Pfs:
             self.data = NestedNamespace(self._data)
 
             # create aliases
-            if hasattr(self.data, "SPECTRAL_WAVE_MODULE"):
-                self.data.SW = self.data.SPECTRAL_WAVE_MODULE
-                self.data.SW.get_outputs = self._get_sw_outputs
+            #if hasattr(self.data, "SPECTRAL_WAVE_MODULE"):
+            #    self.data.SW = self.data.SPECTRAL_WAVE_MODULE
+            #    self.data.SW.get_outputs = self._get_sw_outputs
 
-            if hasattr(self.data, "HYDRODYNAMIC_MODULE"):
-                self.data.HD = self.data.HYDRODYNAMIC_MODULE
-                self.data.HD.get_outputs = self._get_hd_outputs
+            #if hasattr(self.data, "HYDRODYNAMIC_MODULE"):
+            #    self.data.HD = self.data.HYDRODYNAMIC_MODULE
+            #    self.data.HD.get_outputs = self._get_hd_outputs
 
         except Exception:
             raise ValueError(
@@ -109,14 +109,20 @@ class Pfs:
 
             if s[-1] == "]":
                 s = s.replace("]", ":")
+            # check for pipes in filenames
+            if s.count("|") == 2:
+                piped_arg = True
+            else:
+                piped_arg = False
 
+            
         s = s.replace("//", "").replace("|", "")
 
         if len(s) > 0 and s[0] != "!":
             if "=" in s:
                 idx = s.index("=")
 
-                key = s[0:idx]
+                key = s[0:idx]               
                 key = key.strip()
                 value = s[(idx + 1) :]
                 key = key.lower()
@@ -147,155 +153,84 @@ class Pfs:
 
         return adj_line
 
-    # below added for pfs writing capabilities (developed by PIBS)
+    # added by clcr 12.Sept.2022
+    def catch_str_exceptions(self, v):
+        """catch peculiarities of string formatted pfs data
 
-    def write_nested_dict(file_obj, dict_obj, indent):
-        # Iterate over all key-value pairs of dictionary
-        for key, value in dict_obj.items():
-            # If value is dict type, then print nested dict
-            if isinstance(value, dict):
-                file_obj.write(" " * indent + "[" + key + "]\n")
-                print(" " * indent, key, ":", "{")
-                write_nested_dict(file_obj, value, indent + 3)
-                file_obj.write(" " * indent + "EndSect  // " + key + "\n")
+        Args:
+            v (str): value from one pfs line
+
+        Returns:
+            v: modified value
+        """
+        # some crude checks and corrections
+        if isinstance(v, str):
+            # catch scientific notation
+            try:
+                v = float(v)
+            except ValueError:                
+                # add either '' or || as pre- and suffix to strings depending on path definition
+                if v == '':
+                    v = '\'\''
+                elif v[0:2] == '..': # if it begins with . (hinting at relative path, use ||)
+                    v = f'|{v}|'
+                else:
+                    v = f'\'{v}\''
+                    
+        if isinstance(v, datetime):
+            v = v.strftime('%Y, %#m, %#d, %#H, 0, 0') # pfs-datetime output  
+             
+        if isinstance(v, list):
+            v = str(v)[1:-1] # strip [] from lists
+        return v
+
+
+    def write_nested_output(self, f, nested_data, lvl):
+        """
+        similar to pibs write_nested_dict but able to handle pfs nested objects directly
+        Args:
+            f (file object): file object (to write to)
+            nested_data (mikeio.pfs.NestedNamespace): object holding (modified or non-modified data)
+            lvl (int): level of indentation, add a tab \t for each
+        """
+        from types import SimpleNamespace
+        
+        lvl_prefix = '\t' 
+        for k, v in vars(nested_data).items():
+            # check if values are again a namespace instance / new level
+            if isinstance(v, SimpleNamespace):
+                f.write(f"{lvl_prefix * lvl}[{k}]\n")
+                self.write_nested_output(f, v, lvl+1) 
+                f.write(f"{lvl_prefix * lvl}EndSect  // {k}\n\n")
+
             else:
-                file_obj.write(" " * indent + "%s = %s\n" % (key, value))
+                #print(f"{lvl_prefix * lvl}{k} = {v}\n") # JUST FOR TESTING, TO BE REMOVED
 
-    def dict_to_pfs_file(PFS_dict, outputdirectory, outputfilename):
-        if not os.path.exists(outputdirectory):
-            os.makedirs(outputdirectory)
-        outfilepath = outputdirectory + outputfilename
-        with open(outfilepath, "w") as f:
-            write_nested_dict(f, PFS_dict, indent=0)
+                v = self.catch_str_exceptions(v)
+                    
+                # write output                
+                f.write(f"{lvl_prefix * lvl}{k} = {v}\n")
 
-    def PFS_to_dict(SourceFile):
-        # Read source PFS file
-        file1 = open(SourceFile, "r")
-        Lines = file1.readlines()
-        for i in enumerate(Lines):
-            Lines[i[0]] = i[1].strip()
 
-        nr_Levels = 6  # The .sw setup file has only 6 sub levels. OK to leave this hardcoded but it is not ideal for other pfs files
-        LevelNames = [
-            ("Level" + str(i) + "Name") for i in range(1, nr_Levels + 1)
-        ]  # list comprehension to create the names of the levels.
-        columnNames = [
-            "PFS",
-            "Type",
-            "LevelNo",
-            "Key",
-            "KeyName",
-            "Value",
-            "Comment",
-        ] + LevelNames
+    def write(self, fname_out):
+        """
+        similar to pibs write_nested_dict but able to handle pfs nested objects directly
+        Args:
+            fname_out (path): path and filename to write output to
+            lvl (int): level of indentation, add a tab \t for each
+        """
 
-        df = pd.DataFrame(columns=columnNames)
-        df["PFS"] = Lines
-        # Identify type of line in the .sw setup file and populate dataframe
-        for i in df.index:
-            if df.loc[i, "PFS"] == "":
-                df.loc[i, "Type"] = "blank"
-            if df.loc[i, "PFS"].startswith("//"):
-                df.loc[i, "Type"] = "comment"
-            if df.loc[i, "PFS"].startswith("["):
-                df.loc[i, "Type"] = "StartSect"
-            if df.loc[i, "PFS"].startswith("EndSect"):
-                df.loc[i, "Type"] = "EndSect"
-            if "=" in df.loc[i, "PFS"]:
-                df.loc[i, "Type"] = "key_value_pair"
+        with open(fname_out, 'w') as f:
+            # HEADER (TO BE MODIFIED LATER)
+            f.write(f"// Created     : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+            f.write(r"// DLL         : C:\Program Files (x86)\DHI\MIKE Zero\2021\bin\x64\pfs2004.dll")
+            f.write("\n")
+            f.write(r"// Version     : 19.0.0.14309")
+            f.write("\n\n")
+            f.write('[FemEngineSW]\n') 
 
-        # Assign level of dict to dataframe column. At the same time assign the names of the levels to the dataframe.
-        df["LevelNo"] = 0
-        for i in df.index:
-            if i == 0:
-                continue
-            elif df.loc[i, "Type"] == "comment":
-                df.loc[i, "LevelNo"] = df.loc[
-                    i - 1, "LevelNo"
-                ]  # assign level the same as previous
-                df.loc[i, LevelNames] = df.loc[i - 1, LevelNames]
-            elif df.loc[i, "Type"] == "blank":
-                df.loc[i, "LevelNo"] = df.loc[
-                    i - 1, "LevelNo"
-                ]  # assign level the same as previous
-                df.loc[i, LevelNames] = df.loc[i - 1, LevelNames]
-            elif df.loc[i, "Type"] == "key_value_pair":
-                df.loc[i, "LevelNo"] = df.loc[
-                    i - 1, "LevelNo"
-                ]  # assign level the same as previous
-                df.loc[i, LevelNames] = df.loc[i - 1, LevelNames]
-            elif df.loc[i, "Type"] == "StartSect":
-                df.loc[i, "LevelNo"] = (
-                    df.loc[i - 1, "LevelNo"] + 1
-                )  # Add +1 to the level when passing a StartSect
-                df.loc[i, LevelNames] = df.loc[i - 1, LevelNames]
-                df.loc[i, "Level" + str(df.loc[i, "LevelNo"]) + "Name"] = str(
-                    df.loc[i, "PFS"][1:-1]
-                )
-            elif df.loc[i, "Type"] == "EndSect":
-                df.loc[i, LevelNames] = df.loc[i - 1, LevelNames]
-                df.loc[i, "Level" + str(df.loc[i - 1, "LevelNo"]) + "Name"] = float(
-                    "NaN"
-                )
-                df.loc[i, "LevelNo"] = df.loc[i - 1, "LevelNo"] - 1
-
-        # split key value pairs and assign to dataframe
-        for i in df.index:
-            if df.loc[i, "Type"] == "key_value_pair":
-                df.loc[i, "Key"] = df.loc[i, "PFS"].split(" = ")[0]
-                df.loc[i, "Value"] = df.loc[i, "PFS"].split(" = ")[1]
-
-        # Find all comments and assign to dataframe. We dont use this again. Does not seem worth the effort to eventually write it back to the pfs file
-        for i in df.index:
-            if "//" in df.loc[i, "PFS"]:
-                df.loc[i, "Comment"] = df.loc[i, "PFS"].split("//")[1]
-
-        # Add brackets
-        for i in range(1, nr_Levels + 1):
-            if i == 1:
-                df["Level1Name"] = "['" + df["Level1Name"] + "']"
-            else:
-                df["Level" + str(i) + "Name"] = (
-                    "['" + df["Level" + str(i) + "Name"] + "']"
-                ).fillna("")
-        df["Key"] = ("['" + df["Key"] + "']").fillna("")
-
-        df["KeyName"] = (
-            df["Level1Name"]
-            + df["Level2Name"]
-            + df["Level3Name"]
-            + df["Level4Name"]
-            + df["Level5Name"]
-            + df["Level6Name"]
-            + df["Key"]
-        )  # This hardcode to 6 levels command is not ideal, need to look for alternative
-
-        # Start with Hardcoded empty dict, then populate
-        PFS_dict = {}
-        # Select all key value pairs for which the values are NaN. Then create empty dicts
-        df_dropna = df[["KeyName", "Value"]].dropna(
-            subset=["KeyName", "Value"], how="all"
-        )  # drop rows where both key and value is NaN
-        df_keyvalue_dicts = (
-            df_dropna.dropna().drop_duplicates()
-        )  # drop any rows with NaN, and remove duplicates. We are left with only the key value pairs at the deepest level of the sub-dicts
-        df_dictofdicts = df_dropna["KeyName"][
-            df["Value"].isnull()
-        ].drop_duplicates()  # the inverse of df_keyvalue_dicts
-
-        for i in enumerate(
-            df_dictofdicts
-        ):  # We need to print the dicts containing dicts first, otherwise they will write over the keyvalue_dicts. This changes up the pfs file sequence, but tests show that it does not seem to affect how it is read by the Mike engine.
-            exec(
-                "PFS_dict" + i[1] + "= {}"
-            )  # This exec command is not ideal, need to look for alternative
-
-        for i in enumerate(df_keyvalue_dicts["KeyName"].index):
-            exec(
-                "PFS_dict"
-                + df_keyvalue_dicts.loc[i[1], "KeyName"]
-                + ' = r"'
-                + str(df_keyvalue_dicts.loc[i[1], "Value"])
-                + '"'
-            )  # This exec command is not ideal, need to look for alternative
-        return PFS_dict
+            
+            self.write_nested_output(f, self.data, 1)
+            
+            f.write('EndSect')
+    
