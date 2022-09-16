8ecf25fd76ad01a8d3ef8fa6e4e5d461fd34ba5b
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2022-09-16 14:33:07 +0200

    version number

--- mikeio/__init__.py
+++ mikeio/__init__.py
@@ -19,8 +19,8 @@ from platform import architecture
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
 
-__version__ = "1.1.0"
-__dfs_version__: int = 110
+__version__ = "1.2.dev0"
+__dfs_version__: int = 120
 
 
 if "64" not in architecture()[0]:
