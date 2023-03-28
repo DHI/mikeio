788976346a986367f6698a746a9230fbf53b39dd
Henrik Andersson <jan@dhigroup.com>
2023-03-28 14:57:41 +0200

    bad indent

--- mikeio/__init__.py
+++ mikeio/__init__.py
@@ -19,8 +19,8 @@ from platform import architecture
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
 
-#__version__ = "1.5.dev1"  # TODO use git hash instead for dev version?
- __version__ = "1.5.0"
+# __version__ = "1.5.dev1"  # TODO use git hash instead for dev version?
+__version__ = "1.5.0"
 __dfs_version__: int = 150
 
 
@@ -39,7 +39,7 @@ from .spatial import (
     Grid1D,
     Grid2D,
     Grid3D,
-)  
+)
 
 from .xyz import read_xyz
 
