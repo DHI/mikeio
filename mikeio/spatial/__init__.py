4c1825a6963bb07fa58f6affc1b72ad68a620a04
Henrik Andersson <jan@dhigroup.com>
2023-05-03 09:38:16 +0200

    Alias

--- mikeio/spatial/__init__.py
+++ mikeio/spatial/__init__.py
@@ -15,6 +15,6 @@ from ._FM_geometry_layered import (
 from ._grid_geometry import Grid1D, Grid2D, Grid3D
 
 
-from . import _FM_geometry as FM_geometry  # for backward compatibility
+# from . import _FM_geometry as FM_geometry  # for backward compatibility
 
 from ._FM_geometry import GeometryFM  # for backward compatibility
