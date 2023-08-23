e37a258da6b3812fc9db4da901ac45d96c7920f9
Henrik Andersson <jan@dhigroup.com>
2023-08-23 11:58:38 +0200

    Remove WIP

--- mikeio/_time.py
+++ mikeio/_time.py
@@ -12,9 +12,6 @@ class DateTimeSelector:
 
     index: pd.DatetimeIndex
 
-    def __len__(self):
-        return len(self.index)
-
     def isel(
         self,
         x: Optional[
@@ -70,21 +67,3 @@ class DateTimeSelector:
             return [self.isel(t)[0] for t in x]
 
         return indices
-
-
-if __name__ == "__main__":
-
-    idx = pd.date_range("2000-01-01", periods=4, freq="D")
-    assert len(idx) == 4
-
-    dts = DateTimeSelector(idx)
-
-    assert dts.isel(None) == [0, 1, 2, 3]
-    assert dts.isel(0) == [0]
-    assert dts.isel(-1) == [3]
-    assert dts.isel([0, 1]) == [0, 1]
-    assert dts.isel("2000-01-02") == [1]
-    assert dts.isel(["2000-01-02", "2000-01-03"]) == [1, 2]
-    assert dts.isel(idx) == [0, 1, 2, 3]
-    assert dts.isel(slice(1, 4)) == [1, 2, 3]
-    assert dts.isel(slice("2000-01-02", "2000-01-04")) == [1, 2, 3]
