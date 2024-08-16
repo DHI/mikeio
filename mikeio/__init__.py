1a5889ef876c37fca43607fe41a311f7838adbe8
Henrik Andersson <jan@dhigroup.com>
2024-08-16 17:20:48 +0200

    Dev version

--- mikeio/__init__.py
+++ mikeio/__init__.py
@@ -21,7 +21,7 @@ from typing import Any
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 #
 
-__version__ = "2.0.1"  # TODO use git hash instead for dev version?
+__version__ = "2.1.dev1"  # TODO use git hash instead for dev version?
 # __version__ = "1.5.0"
 __dfs_version__: int = 200
 
