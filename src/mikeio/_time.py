21a7ab3b44555eab93f9bffe08de983a8e49e22c
Henrik Andersson <jan@dhigroup.com>
2025-10-11 21:31:23 +0200

    match in DatetimeSelector

--- src/mikeio/_time.py
+++ src/mikeio/_time.py
@@ -21,11 +21,12 @@ def _get_time_idx_list(
     # indexing with a slice needs to be handled differently, since slicing returns a view
 
     if isinstance(steps, slice):
-       if isinstance(steps.start, int) and isinstance(steps.stop, int):
-         return steps
+        if isinstance(steps.start, int) and isinstance(steps.stop, int):
+            return steps
 
     dts = DateTimeSelector(time)
     return dts.isel(steps)
+
 
 @dataclass
 class DateTimeSelector:
@@ -35,15 +36,16 @@ class DateTimeSelector:
 
     def isel(
         self,
-        x: (
+        key: (
             int | Iterable[int] | str | datetime | pd.DatetimeIndex | slice | None
         ) = None,
+        /,
     ) -> list[int]:
         """Select time steps from a pandas DatetimeIndex.
 
         Parameters
         ----------
-        x : int, Iterable[int], str, datetime, pd.DatetimeIndex, slice
+        key : int, Iterable[int], str, datetime, pd.DatetimeIndex, slice
             Time steps to select, negative indices are supported
 
         Returns
@@ -71,27 +73,32 @@ class DateTimeSelector:
         """
         indices = list(range(len(self.index)))
 
-        if x is None:
-            return indices
+        match key:
+            case None:
+                return indices
 
-        if isinstance(x, int):
-            return [indices[x]]
+            case int():
+                return [indices[key]]
 
-        if isinstance(x, (datetime, str)):
-            loc = self.index.get_loc(x)
-            if isinstance(loc, int):
-                return [loc]
-            elif isinstance(loc, slice):
-                return list(range(loc.start, loc.stop))
+            case datetime() | str():
+                loc = self.index.get_loc(key)
+                match loc:
+                    case int():
+                        return [loc]
+                    case slice():
+                        return list(range(loc.start, loc.stop))
 
-        if isinstance(x, slice):
-            if isinstance(x.start, int) or isinstance(x.stop, int):
-                return indices[x]
-            else:
-                s = self.index.slice_indexer(x.start, x.stop)
-                return list(range(s.start, s.stop))
+            case slice():
+                if isinstance(key.start, int) or isinstance(key.stop, int):
+                    return indices[key]
+                else:
+                    s = self.index.slice_indexer(key.start, key.stop)
+                    return list(range(s.start, s.stop))
 
-        if isinstance(x, Iterable):
-            return [self.isel(t)[0] for t in x]
+            case Iterable():
+                return [self.isel(t)[0] for t in key]
+
+            case _:
+                return indices
 
         return indices
